0c49e851ccccd1a628ec637e00f53454616dbb78
jaschluc732 <62405775+jaschluc732@users.noreply.github.com>
2020-10-01 18:41:44 -0700

    Update C_Program.c

--- Lesson-02/C_Program.c
+++ Lesson-02/C_Program.c
@@ -2,7 +2,7 @@
 
 int main(){
 
-print("Hello There Again you guys!!! I am C-Program waffle house. Thank you too! and come back again tomorrow");
+print("Hello There Again you guys!!! I am C-Program waffle house. Thank you too! and come back again tomorrow you guys");
 
 retrun 0;
 
