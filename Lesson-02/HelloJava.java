9bce89416e3e511b3fe3136608d7b3f9cb0a63a4
Jack Schluckbier <jaschluc@cisco.com>
2020-11-22 09:15:28 -0800

    Revert "changed java"
    This reverts commit 117bace2f4b608edb5c475ae4f3abba293b90c8c.

--- Lesson-02/HelloJava.java
+++ Lesson-02/HelloJava.java
@@ -1,5 +1,5 @@
 class HelloJava {
 public static void main(String args[]){
-System.out.Println(“ I am your Java Program for tonight agian. Thank you very much for checking me out! ”);
+System.out.Println(“ I am your Java Program for tonight. Thank you very much for checking me out! ”);
 }
 }
